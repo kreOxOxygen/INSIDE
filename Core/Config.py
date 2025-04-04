842c8e20262204baf7fb9b09c8bff58dcaabd250
kreOxOxygen <43717866+kreOxOxygen@users.noreply.github.com>
2025-04-04 15:07:10 +0300

    Update Config.py
    removed URL_CHANNEL, tg will not open never

--- Core/Config.py
+++ Core/Config.py
@@ -8,9 +8,6 @@ VERSION = '1.07'
 # Стандартный цвет программы
 MY_COLOR = 'deeppurpleaccent100'
 
-# Ссылка на канал
-URL_CHANNEL = 'https://127.0.0.1:9876'
-
 # Название конфига
 CONFIG_NAME = 'config.json'
 
