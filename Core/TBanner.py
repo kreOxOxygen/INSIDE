b8542183e405f03be049f7d3e4ebd1a5d4bc3ab6
kreOxOxygen <43717866+kreOxOxygen@users.noreply.github.com>
2025-04-04 15:26:47 +0300

    Update TBanner.py

--- Core/TBanner.py
+++ Core/TBanner.py
@@ -1,6 +1,12 @@
 from colorama import Fore
 
 
+_banner1 = '''
+88  88 888888 88     88      dP"Yb 
+88  88 88__   88     88     dP   Yb 
+888888 88""   88  .o 88  .o Yb   dP 
+88  88 888888 88ood8 88ood8  YbodP
+'''
 
 _banner = '''
  88888  dP"Yb   dP""b8 88  dP 888888 88""Yb 
@@ -13,6 +19,6 @@ o.  88 Yb   dP Yb      88"Yb  88""   88"Yb
 
 def banner(host, port):
     '''Вывод баннера с ссылкой'''
-
+    print(Fore.MAGENTA + _banner1)
     print(Fore.MAGENTA + _banner)
     print(f'Перейдите по http://{host}:{port}')
