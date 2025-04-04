6c7d37f8a11c9926a3d622e5f7554b73b2c8ba99
kreOxOxygen <43717866+kreOxOxygen@users.noreply.github.com>
2025-04-04 15:23:37 +0300

    Update TBanner.py

--- Core/TBanner.py
+++ Core/TBanner.py
@@ -3,10 +3,10 @@ from colorama import Fore
 
 
 _banner = '''
-88  88 888888 88     88      dP"Yb      8888b.     db    8888b.  8888b.  Yb  dP 
-88  88 88__   88     88     dP   Yb      8I  Yb   dPYb    8I  Yb  8I  Yb  YbdP
-888888 88""   88  .o 88  .o Yb   dP      8I  dY  dP__Yb   8I  dY  8I  dY   8P 
-88  88 888888 88ood8 88ood8  YbodP      8888Y"  dP""""Yb 8888Y"  8888Y"   dP 
+ 88888  dP"Yb   dP""b8 88  dP 888888 88""Yb 
+    88 dP   Yb dP   `" 88odP  88__   88__dP 
+o.  88 Yb   dP Yb      88"Yb  88""   88"Yb
+"bodP'  YbodP   YboodP 88  Yb 888888 88  Yb 
 '''
 
 
